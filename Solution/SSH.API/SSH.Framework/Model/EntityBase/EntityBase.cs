ae783f3afde73b45ea58f5d9becfe2380a8a3150
jaypatwa6 <jaypatwa6192@gmail.com>
2025-12-04 17:04:23 +0530

    Prepared EntityBase and IRepositoryBase

---
+++ Solution/SSH.API/SSH.Framework/Model/EntityBase/EntityBase.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SSH.Framework.Model.EntityBase
+{
+    public abstract class EntityBase
+    {
+    }
+
+    public abstract class Entity<TIdentityType> : EntityBase
+    {
+        [Key]
+        public TIdentityType Id { get; set; }
+
+        #region Idenity Management
+
+        public override bool Equals(object? obj)
+        {
+            if (this == obj)
+                return true;
+
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            var other = (Entity<TIdentityType>)obj;
+            return base.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        #endregion
+    }
+
+    public abstract class Entity : Entity<Guid>
+    {
+
+    }
+
+    public abstract class LongEntity : Entity<long>
+    {
+
+    }
+}
---
+++ Solution/SSH.API/SSH.Framework/Persistence/Repository/IRepositoryBase.cs
@@ -0,0 +1,27 @@
+﻿using SSH.Framework.Model.EntityBase;
+using System.Collections.ObjectModel;
+using System.Linq.Expressions;
+
+namespace SSH.Framework.Persistence.Repository
+{
+    public interface IRepositoryBase<TEntity, in TKey> where TEntity : Entity<TKey>
+    {
+        bool IsTracking { set; }
+
+        Collection<Expression<Func<TEntity, object>>> Includes { get; }
+
+        TEntity GetOneById(TKey id);
+
+        TEntity GetOne(Expression<Func<TEntity, bool>> predicate);
+
+        TEntity GetOne<TMappedEntity>(Expression<Func<TEntity, TMappedEntity>> selector, Expression<Func<TEntity, object>> predicate);
+
+        IEnumerable<TEntity> GetAll();
+
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate = null);
+
+        int Count(Expression<Func<TEntity, bool>> predicate = null);
+
+        bool CheckExist(Expression<Func<TEntity, bool>> predicate = null);
+    }
+}
